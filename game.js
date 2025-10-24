594d6730a8b30411219335431339faaf63182c9a
nobody-alive <ayaansh.kochhar@outlook.com>
2025-10-24 13:54:09 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,15 +1,4 @@
 // Phaser 3 Adventure - v2
-const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  physics: {
-    default: 'arcade',
-    arcade: { gravity: { y: 1000 }, debug: false }
-  },
-  scene: [BootScene, Level1, Level2, GameOverScene]
-};
-
 
 // ---- Boot Scene ----
 class BootScene extends Phaser.Scene {
